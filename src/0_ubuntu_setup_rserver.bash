84ef900037ba7ecd060f2d3f3bbe42ac99adf43b
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2024-07-15 16:34:53 -0500

    Update 0_ubuntu_setup_rserver.bash

--- src/0_ubuntu_setup_rserver.bash
+++ src/0_ubuntu_setup_rserver.bash
@@ -1,1 +1,2 @@
+wget -O - https://raw.githubusercontent.com/mmvvff/aamne_decompr/main/src/0_ubuntu_setup/0_ubuntu_setup_data.bash | bash
 wget -O - https://raw.githubusercontent.com/mmvvff/r_ubuntu/main/bash_scripts/rubuntu_0.sh | bash

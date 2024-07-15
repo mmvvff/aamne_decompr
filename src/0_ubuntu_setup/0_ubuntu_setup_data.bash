3369c197d2cfcb10d952c32b6b5a3aafe6d8ff43
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2024-07-15 15:43:35 -0500

    reorganize folders

--- src/0_ubuntu_setup/0_ubuntu_setup_data.bash
+++ src/0_ubuntu_setup/0_ubuntu_setup_data.bash
@@ -6,3 +6,7 @@ sudo mkdir -p 0_scripts
 
 # folder setpup
 sudo mkdir -p 1_data 2_pipeline 3_output
+
+cd ~/0_scripts
+[ -f url_aamne23.txt ] && sudo rm url_aamne23.txt
+sudo wget -O url_aamne23.txt https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/python_req_geopandas.txt

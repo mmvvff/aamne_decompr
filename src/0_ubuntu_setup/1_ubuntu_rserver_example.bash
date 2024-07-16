e7b735ad8fad6fae08f153b7393f247804d7a638
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2024-07-16 17:51:51 -0500

    modify for batch tests

--- src/0_ubuntu_setup/1_ubuntu_rserver_example.bash
+++ src/0_ubuntu_setup/1_ubuntu_rserver_example.bash
@@ -5,7 +5,7 @@ sudo wget -O codes_sector_oecd_aamneV23_classification.csv https://raw.githubuse
 
 cd ~/0_scripts
 sudo wget -O 1_prepdata_prll.R https://raw.githubusercontent.com/mmvvff/aamne_decompr/main/src/1_prepdata_prll.R
-sudo wget -O 2_decompr_cousec_sngl.R https://raw.githubusercontent.com/mmvvff/aamne_decompr/main/src/cousec/2_decompr_cousec_sngl.R
+sudo wget -O 2_decompr_cousec_sngl_0002.R https://raw.githubusercontent.com/mmvvff/aamne_decompr/main/src/cousec/2_decompr_cousec_sngl.R
 
 cd ~
 sudo Rscript ~/0_scripts/1_prepdata_prll.R

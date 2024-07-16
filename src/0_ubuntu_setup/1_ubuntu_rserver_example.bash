e37cd2fbbabd726802d54d6d50ab94bbc853bc51
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2024-07-16 12:20:40 -0500

    Update 1_ubuntu_rserver_example.bash

--- src/0_ubuntu_setup/1_ubuntu_rserver_example.bash
+++ src/0_ubuntu_setup/1_ubuntu_rserver_example.bash
@@ -1,1 +1,3 @@
-sudo wget -O r_requirements.R https://raw.githubusercontent.com/mmvvff/aamne_decompr/main/src/0_ubuntu_setup/src_ubuntu_rserver/r_requirements.R
+cd ~/0_scripts
+sudo wget -O 1_prepdata_prll.R https://raw.githubusercontent.com/mmvvff/aamne_decompr/main/src/1_prepdata_prll.R
+cd ~ && sudo Rscript ~/0_scripts/1_prepdata_prll.R

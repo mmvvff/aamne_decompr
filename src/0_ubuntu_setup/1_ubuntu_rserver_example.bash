b87162ba254bc9f5e92c66424e456b3722866f0d
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2024-07-16 12:36:27 -0500

    Update 1_ubuntu_rserver_example.bash

--- src/0_ubuntu_setup/1_ubuntu_rserver_example.bash
+++ src/0_ubuntu_setup/1_ubuntu_rserver_example.bash
@@ -1,3 +1,8 @@
+cd ~/r_aamne_wwz/0_data
+sudo wget -O codes_sector_oecd_aamneV18_classification.csv https://raw.githubusercontent.com/mmvvff/aamne_decompr/main/data_urls/codes_sector_oecd_aamneV18_classification.csv
+sudo wget -O codes_sector_oecd_aamneV23_classification.csv https://raw.githubusercontent.com/mmvvff/aamne_decompr/main/data_urls/codes_sector_oecd_aamneV23_classification.csv
+
+
 cd ~/0_scripts
 sudo wget -O 1_prepdata_prll.R https://raw.githubusercontent.com/mmvvff/aamne_decompr/main/src/1_prepdata_prll.R
 sudo wget -O 2_decompr_cousec_prll.R https://raw.githubusercontent.com/mmvvff/aamne_decompr/main/src/cousec/2_decompr_cousec_prll.R

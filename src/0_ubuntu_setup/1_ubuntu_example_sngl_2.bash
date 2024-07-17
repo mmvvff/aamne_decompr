89bd007969500dd08a618062e2e5290590f76b0b
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2024-07-17 09:41:08 -0500

    re-structure folders

--- src/0_ubuntu_setup/1_ubuntu_example_sngl_2.bash
+++ src/0_ubuntu_setup/1_ubuntu_example_sngl_2.bash
@@ -1,5 +1,5 @@
 cd ~/0_scripts
-sudo wget -O 2_run_decompr_scripts https://raw.githubusercontent.com/mmvvff/aamne_decompr/main/src/cousec/2_run_decompr_scripts
+sudo wget -O 2_run_decompr_scripts.sh https://raw.githubusercontent.com/mmvvff/aamne_decompr/main/src/cousec/helpers/2_run_decompr_scripts.sh
 chmod +x run_decompr_scripts.sh
 cd ~
 ~/0_scripts/2_run_decompr_scripts.sh

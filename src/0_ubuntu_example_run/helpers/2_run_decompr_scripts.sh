44b3e6cbcd688b499b3c7893e7e3da1b5ffca541
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2024-07-17 10:48:26 -0500

    Update 2_run_decompr_scripts.sh

--- src/0_ubuntu_example_run/helpers/2_run_decompr_scripts.sh
+++ src/0_ubuntu_example_run/helpers/2_run_decompr_scripts.sh
@@ -2,13 +2,13 @@
 
 # Array of script names
 scripts=(
-    "2_decompr_cousec_sngl_00-01.R"
-    "2_decompr_cousec_sngl_02-03.R"
-    "2_decompr_cousec_sngl_04-05.R"
-    "2_decompr_cousec_sngl_06-07.R"
-    "2_decompr_cousec_sngl_08-09.R"
-    "2_decompr_cousec_sngl_10-11.R"
-    "2_decompr_cousec_sngl_12-13.R"
+    "2_decompr_cousec_sngl_btch_00-01.R"
+    "2_decompr_cousec_sngl_btch_02-03.R"
+    "2_decompr_cousec_sngl_btch_04-05.R"
+    "2_decompr_cousec_sngl_btch_06-07.R"
+    "2_decompr_cousec_sngl_btch_08-09.R"
+    "2_decompr_cousec_sngl_btch_10-11.R"
+    "2_decompr_cousec_sngl_btch_12-13.R"
 )
 
 # Loop through the scripts and run them

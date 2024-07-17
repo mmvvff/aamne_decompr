7fb19c8933fb9ab071cf4a3462e43148f89a0e8c
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2024-07-17 10:03:10 -0500

    update single batch processing

--- src/0_ubuntu_example_run/1_ubuntu_example_sngl.bash
+++ src/0_ubuntu_example_run/1_ubuntu_example_sngl.bash
@@ -4,6 +4,7 @@ sudo wget -O codes_sector_oecd_aamneV18_classification.csv https://raw.githubuse
 sudo wget -O codes_sector_oecd_aamneV23_classification.csv https://raw.githubusercontent.com/mmvvff/aamne_decompr/main/data_urls/codes_sector_oecd_aamneV23_classification.csv
 
 # for single batch processing
+# download scripts
 cd ~/0_scripts
 sudo wget -O 2_decompr_cousec_sngl_btch_00-01.R https://raw.githubusercontent.com/mmvvff/aamne_decompr/main/src/cousec/2_decompr_cousec_sngl_btch_00-01.R
 sudo wget -O 2_decompr_cousec_sngl_btch_02-03.R https://raw.githubusercontent.com/mmvvff/aamne_decompr/main/src/cousec/2_decompr_cousec_sngl_btch_02-03.R
@@ -13,3 +14,15 @@ sudo wget -O 2_decompr_cousec_sngl_btch_08-09.R https://raw.githubusercontent.co
 sudo wget -O 2_decompr_cousec_sngl_btch_10-11.R https://raw.githubusercontent.com/mmvvff/aamne_decompr/main/src/cousec/2_decompr_cousec_sngl_btch_04-11.R
 sudo wget -O 2_decompr_cousec_sngl_btch_12-13.R https://raw.githubusercontent.com/mmvvff/aamne_decompr/main/src/cousec/2_decompr_cousec_sngl_btch_12-13.R
 sudo wget -O 2_decompr_cousec_sngl_btch_end.R https://raw.githubusercontent.com/mmvvff/aamne_decompr/main/src/cousec/2_decompr_cousec_sngl_btch_end.R
+
+# run scripts in parallel: minimum requiered CPU with 8 cores
+cd ~/0_scripts
+sudo wget -O 2_run_decompr_scripts.sh https://raw.githubusercontent.com/mmvvff/aamne_decompr/main/src/cousec/helpers/2_run_decompr_scripts.sh
+chmod +x run_decompr_scripts.sh
+cd ~
+~/0_scripts/2_run_decompr_scripts.sh
+
+# collect output from scripts
+cd ~
+nohup Rscript ~/0_scripts/2_decompr_cousec_sngl_btch_end.R > ~/0_scripts/2_decompr_cousec_sngl_btch_end_output.log 2>&1 &
+# nohup: to avoid that the r scripts stops becuase it looses connection
--- src/0_ubuntu_example_run/1_ubuntu_example_sngl_2.bash
+++
@@ -1,5 +0,0 @@
-cd ~/0_scripts
-sudo wget -O 2_run_decompr_scripts.sh https://raw.githubusercontent.com/mmvvff/aamne_decompr/main/src/cousec/helpers/2_run_decompr_scripts.sh
-chmod +x run_decompr_scripts.sh
-cd ~
-~/0_scripts/2_run_decompr_scripts.sh

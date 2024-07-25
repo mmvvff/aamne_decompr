71495b3198b50e62bb885ee24ba74c09f5b7fe46
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2024-07-25 12:21:21 -0500

    Update 2_ubuntu_eg_indexed_prll.bash

--- src/0_ubuntu_example_run/2_ubuntu_eg_indexed_prll.bash
+++ src/0_ubuntu_example_run/2_ubuntu_eg_indexed_prll.bash
@@ -2,10 +2,10 @@
 
 # for parallel processing
 cd ~/0_scripts
-[ -f 1_preparedata_indexed_prll ] && sudo rm 1_preparedata_indexed_prll
-sudo wget -O 1_preparedata_indexed_prll https://raw.githubusercontent.com/mmvvff/aamne_decompr/main/src/1_transform_indexed_example/1_preparedata_indexed_prll
+[ -f 1_trnsfrm_indexed_prll ] && sudo rm 1_trnsfrm_indexed_prll
+sudo wget -O 1_trnsfrm_indexed_prll https://raw.githubusercontent.com/mmvvff/aamne_decompr/main/src/1_transform_indexed_example/1_trnsfrm_indexed_prll
 
-[ -f 1_preparedata_indexed_prll.log ] && sudo rm 1_preparedata_indexed_prll.log
+[ -f 1_trnsfrm_indexed_prll.log ] && sudo rm 1_trnsfrm_indexed_prll.log
 cd ~
 # to avoid that the r scripts stops becuase it looses connection:
-nohup Rscript ~/0_scripts/2_decompr_cousec_prll.R > ~/0_scripts/1_preparedata_indexed_prll.log 2>&1 &
+nohup Rscript ~/0_scripts/2_decompr_cousec_prll.R > ~/0_scripts/1_trnsfrm_indexed_prll.log 2>&1 &

1db082391e7802b33175bbb3a4b7cb43b6c370d4
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2024-07-25 12:29:32 -0500

    Update 2_ubuntu_eg_indexed_prll.bash

--- src/0_ubuntu_example_run/2_ubuntu_eg_indexed_prll.bash
+++ src/0_ubuntu_example_run/2_ubuntu_eg_indexed_prll.bash
@@ -2,8 +2,8 @@
 
 # for parallel processing
 cd ~/0_scripts
-[ -f 1_trnsfrm_indexed_prll ] && sudo rm 1_trnsfrm_indexed_prll
-sudo wget -O 1_trnsfrm_indexed_prll https://raw.githubusercontent.com/mmvvff/aamne_decompr/main/src/1_transform_indexed_example/1_trnsfrm_indexed_prll.R
+[ -f 1_trnsfrm_indexed_prll.R ] && sudo rm 1_trnsfrm_indexed_prll.R
+sudo wget -O 1_trnsfrm_indexed_prll.R https://raw.githubusercontent.com/mmvvff/aamne_decompr/main/src/1_transform_indexed_example/1_trnsfrm_indexed_prll.R
 
 [ -f 1_trnsfrm_indexed_prll.log ] && sudo rm 1_trnsfrm_indexed_prll.log
 cd ~

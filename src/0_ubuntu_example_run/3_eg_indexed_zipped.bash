fa1e7ea28625a9fcf8c9a1f5dd27b273f0fd7fa1
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2024-07-26 18:35:58 -0500

    Update 3_eg_indexed_zipped.bash

--- src/0_ubuntu_example_run/3_eg_indexed_zipped.bash
+++ src/0_ubuntu_example_run/3_eg_indexed_zipped.bash
@@ -17,7 +17,7 @@ cd ~/zipped
 
 # Compress the project directory using tar and pigz (parallel gzip)
 # The -9 flag sets maximum compression, and -p $cores specifies the number of cores to use
-tar cf - ~/r_aamne_wwz/2_pipeline/R_aamne_indexed | pigz -9 -p $cores > R_aamne_indexed.tar.gz 2
+tar cf - ~/r_aamne_wwz/2_pipeline/R_aamne_indexed | pigz -9 -p $cores > R_aamne_indexed.tar.gz
 
 # Check if the compressed file exists and has content
 if [ -s R_aamne_indexed.tar.gz ]; then
@@ -26,3 +26,9 @@ else
     echo "Compression failed"
     exit 1
 fi
+
+# to copy individual file
+# gsutil cp ~/zipped/R_aamne_indexed.tar.gz gs://zipped2download/
+
+# to sync all contents of folder
+# gsutil -m rsync -r ~/zipped/ gs://zipped2download/
